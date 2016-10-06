0996bf23f58901ea06a3e22112e02acb6f47f6b0
9Kevinzhao <9kzhao@stpatrick.org>
2016-10-06 14:11:51 -0500

    started working

---
+++ octocats.js
@@ -0,0 +1,15 @@
+//Write your constructor function below.
+//You should have 2 instance variables and 1 instance function included.
+
+funciton octocat(N,A)
+var octocat.Name = N
+var Octocat.Arms = A
+var Octocat.Slap = function {
+  for(var i=0;i<A;i++)
+}
+
+
+
+
+
+//Write your 3 new octocat objects below here.
